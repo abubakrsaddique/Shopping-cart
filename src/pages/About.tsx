cf3593ebc73567b75097dae1308be068787db800
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-07-18 18:04:00 +0500

    final commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+export function About() {
+  return (
+    <div
+      className="container d-flex justify-content-center align-items-center "
+      style={{
+        height: "75vh",
+      }}
+    >
+      <div className="text-center">
+        <h1 className="text-primary">
+          Our accomplishments are a result of hard work, team spirit and
+          determination
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export function Home() {
+  return (
+    <div
+      className="container d-flex justify-content-center align-items-center "
+      style={{
+        height: "75vh",
+      }}
+    >
+      <div className="text-center">
+        <h1 className="text-primary">Welcome to Our Shopping Site</h1>
+      </div>
+    </div>
+  );
+}
